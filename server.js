ae8d8111666e24a32d8bb9024db450bed60b228e
MichelPodkhvatilin <podkhvatilin.work@gmail.com>
2021-02-01 21:52:43 +0200

    add eslint and prettier

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const corsConfig = require("./config/cors.config");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const corsConfig = require('./config/cors.config');
 
 const app = express();
 
@@ -9,18 +9,17 @@ app.use(cors(corsConfig));
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-    res.json({message: "Welcome to test application."});
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to test application.' });
 });
 
-app.post("/", (req, res) => {
-    console.log(req.body)
-    res.json({message: "User was registered successfully!"});
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.json({ message: 'User was registered successfully!' });
 });
-
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
