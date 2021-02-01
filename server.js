b3f1679b5135f42d8cb5c81330b2e06752798f4e
MichelPodkhvatilin <podkhvatilin.work@gmail.com>
2021-02-01 13:59:53 +0200

    add base express app config

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const corsConfig = require("./config/cors.config");
+
+const app = express();
+
+app.use(cors(corsConfig));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.json({message: "Welcome to test application."});
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body)
+    res.json({message: "User was registered successfully!"});
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
