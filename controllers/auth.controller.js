22b6773fff8e880187deefdbc9e9219d3be36e72
MichelPodkhvatilin <podkhvatilin.work@gmail.com>
2021-02-15 20:45:20 +0200

    refactor signin controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -69,52 +69,58 @@ exports.signup = (req, res) => {
   });
 };
 
-exports.signin = (req, res) => {
-  UserModel.findOne({
-    username: req.body.username,
-  })
-    .populate('roles', '-__v')
-    .exec((err, user) => {
-      if (err) {
-        res.status(500).send({ message: err });
-        return;
-      }
+exports.signin = async (req, res) => {
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
 
-      if (!user) {
-        res.status(404).send({ message: 'User Not found.' });
-        return;
-      }
+    if (!username) {
+      res.status(400).send({ message: 'Username is not entered!' });
+      return;
+    }
 
-      const passwordIsValid = bcrypt.compareSync(
-        req.body.password,
-        user.password
-      );
+    if (!password) {
+      res.status(400).send({ message: 'Password is not entered!' });
+      return;
+    }
 
-      if (!passwordIsValid) {
-        res.status(401).send({
-          accessToken: null,
-          message: 'Invalid Password!',
-        });
-        return;
-      }
+    const user = await UserModel.findOne({ username })
+      .populate('roles', '-__v')
+      .exec();
 
-      const token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400, // 24 hours
+    if (!user) {
+      res.status(404).send({ message: 'User Not found.' });
+      return;
+    }
+
+    const passwordIsValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordIsValid) {
+      res.status(401).send({
+        accessToken: null,
+        message: 'Invalid Password!',
       });
+      return;
+    }
 
-      RoleModel.findById({ _id: user.role }, (err, role) => {
-        if (err) {
-          res.status(500).send({ message: err });
-          return;
-        }
-
-        res.status(200).send({
-          id: user._id,
-          username: user.username,
-          email: user.email,
-          role: role.name,
-          accessToken: token,
-        });
-      });
+    const token = jwt.sign({ id: user.id }, config.secret, {
+      expiresIn: 86400, // 24 hours
     });
+
+    const userRoleId = user.role;
+    const role = await RoleModel.findById({ _id: userRoleId }).exec();
+
+    res.status(200).send({
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      role: role.name,
+      accessToken: token,
+    });
+  } catch (error) {
+    res.status(500).send({ message: error });
+  }
 };
