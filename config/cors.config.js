1e96647ab0b0ac5f8c281093d734bb2dba6f4f42
MichelPodkhvatilin <podkhvatilin.work@gmail.com>
2021-02-16 21:10:49 +0200

    change cors config

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  origin: 'http://localhost:8080',
+const whitelist = ['http://localhost:8080'];
+const corsConfig = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
 };
+
+module.exports = corsConfig;
