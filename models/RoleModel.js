d680b6d3bf816b05d2215f442095d51b851092b7
MichelPodkhvatilin <podkhvatilin.work@gmail.com>
2021-02-25 22:58:09 +0200

    change user schema and admin init func

--- models/RoleModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const RoleSchema = require('./schemas/RoleSchema');
-
-const RoleModel = mongoose.model('role', RoleSchema);
-
-module.exports = RoleModel;
--- models/schemas/UserSchema.js
+++ models/schemas/UserSchema.js
@@ -1,25 +1,57 @@
+const validator = require('validator');
 const Schema = require('mongoose').Schema;
-const ProfileSchema = require('./ProfileSchema');
 
 const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: 1,
-  },
   email: {
     type: String,
+    unique: true,
     required: true,
-    unique: 1,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email',
+      isAsync: false,
+    },
   },
   password: {
     type: String,
     required: true,
   },
-  profile: ProfileSchema,
   role: {
-    type: Schema.Types.ObjectId,
-    ref: 'role',
+    type: String,
+    required: true,
+    enum: ['admin', 'master', 'user'],
+  },
+  profile: {
+    firstName: {
+      type: String,
+      default: 'FirstName',
+      validate: {
+        validator: (value) => {
+          return value.trim().length;
+        },
+        message: 'This is not a valid firstname!',
+        isAsync: false,
+      },
+    },
+    lastName: {
+      type: String,
+      default: 'LastName',
+      validate: {
+        validator: (value) => {
+          return value.trim().length;
+        },
+        message: 'This is not a valid lastname!',
+        isAsync: false,
+      },
+    },
+    phone: {
+      type: String,
+      validate: {
+        validator: validator.isMobilePhone,
+        message: '{VALUE} is not a valid lastname!',
+        isAsync: false,
+      },
+    },
   },
 });
 
