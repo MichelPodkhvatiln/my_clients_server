343eebaa066fe7fcc1e52dd4dfa49e53045895eb
MichelPodkhvatilin <podkhvatilin.work@gmail.com>
2021-02-27 20:48:28 +0200

    add service route and controller

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const routes = require('express').Router();
 routes.use('/auth', require('./auth.routes'));
 routes.use('/user', require('./user.routes'));
 routes.use('/salon', require('./salon.routes'));
+routes.use('/service', require('./service.routes'));
 
 module.exports = routes;
